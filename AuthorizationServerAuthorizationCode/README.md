06c574dba7fabd37e7d3b1f37810131bbf2f9532
AnatolSalau <salovanatoly5@mail.ru>
2024-05-21 11:28:17 +0300

    Add AuthorizationServerCodeAndClientApplication

---
+++ AuthorizationServerAuthorizationCode/README.md
@@ -0,0 +1,4 @@
+https://www.youtube.com/watch?v=QyKOLZjpS5w&ab_channel=SpringBootTUTORIAL.
+
+Oauth debugger
+https://oauthdebugger.com/
---
+++ AuthorizationServerCodeAndClient/src/main/java/com/example/authorizationservercodeandclient/AuthorizationServerCodeAndClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorizationservercodeandclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerCodeAndClientApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(AuthorizationServerCodeAndClientApplication.class, args);
+      }
+
+}
