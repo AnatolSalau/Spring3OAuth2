0f8e5a3c451932021aaf5ab7b532e45e1ea9c7bc
AnatolSalau <salovanatoly5@mail.ru>
2024-05-22 16:05:05 +0300

    Add ClientServerClientCredentialApplication

---
+++ ClientServerClientCredential/src/main/java/com/example/clientserverclientcredential/ClientServerClientCredentialApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientserverclientcredential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServerClientCredentialApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(ClientServerClientCredentialApplication.class, args);
+      }
+
+}
